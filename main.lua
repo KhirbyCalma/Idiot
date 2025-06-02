4cf7145f82992ec18150bff84ba29a6126f05e08
Khirby Calma <95603154+KhirbyCalma@users.noreply.github.com>
2025-06-01 21:29:03 -0700

    Set game window title

--- main.lua
+++ main.lua
@@ -1,13 +1,5 @@
 local love = require("love")
 
-function love.draw()
-    love.graphics.print("Hello World!", 400, 300)
-end
-
--- load resources like initializing variables, settings, etc.
 function love.load()
-    image = love.graphics.newImage("Blueprint.png")
-    love.graphics.setNewFont(12)
-    love.graphics.setColor(0, 0, 0)
-    love.graphics.setBackgroundColor(255, 255, 255)
+    love.window.setTitle("Idiot")
 end 

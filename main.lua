89efafc8b5a90ed87eb625eddc593cbe045f2c52
Khirby Calma <95603154+KhirbyCalma@users.noreply.github.com>
2025-06-01 21:17:15 -0700

    Implement load callback function to be called when game loads

--- main.lua
+++ main.lua
@@ -3,3 +3,11 @@ local love = require("love")
 function love.draw()
     love.graphics.print("Hello World!", 400, 300)
 end
+
+-- load resources like initializing variables, settings, etc.
+function love.load()
+    image = love.graphics.newImage("Blueprint.png")
+    love.graphics.setNewFont(12)
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.setBackgroundColor(255, 255, 255)
+end 

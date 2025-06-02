2460ab0f9e2d03a531a7fe03b481fc0f50d8ea87
Khirby Calma <95603154+KhirbyCalma@users.noreply.github.com>
2025-06-01 21:36:03 -0700

    Implement game window icon

--- main.lua
+++ main.lua
@@ -2,4 +2,6 @@ local love = require("love")
 
 function love.load()
     love.window.setTitle("Idiot")
+    local imageData = love.image.newImageData("GameIcon.png")
+    love.window.setIcon(imageData)
 end 

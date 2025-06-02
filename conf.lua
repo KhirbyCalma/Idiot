8a2334326b71009a7714ff18931057a23611b324
Khirby Calma <95603154+KhirbyCalma@users.noreply.github.com>
2025-06-02 07:18:19 -0700

    Move window title and icon logic to conf file

---
+++ conf.lua
@@ -0,0 +1,5 @@
+local love = require "love"
+function love.conf(app)
+    app.window.title = "Idiot"
+    app.window.icon = "GameIcon.png"
+end 
--- main.lua
+++ main.lua
@@ -1,7 +1,13 @@
 local love = require("love")
 
 function love.load()
-    love.window.setTitle("Idiot")
-    local imageData = love.image.newImageData("GameIcon.png")
-    love.window.setIcon(imageData)
+
 end 
+
+function love.update(dt)
+
+end
+
+function love.draw()
+
+end
